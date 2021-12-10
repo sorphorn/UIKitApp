9e395d6c86117f461350077eeea73507613ee4c1
Sorphorn <so.sorphorn@gmail.com>
2021-12-10 14:59:48 +0700

    First init

---
+++ MyApp/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MyApp
+//
+//  Created by MacbookPro on 12/9/21.
+//
+
+import UIKit
+import MyQuestionairFramework
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+        print("Show the message: ",MyQuestionair.question())
+    
+    }
+
+
+}
+
